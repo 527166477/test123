7b1ce2a228a413410f6b7b69e48014c25149a3dd
yiruit <yiruit@yiruitdeMac-mini.local>
2017-03-10 16:12:18 +0800

    测试使用git

--- Test副本/Test/JJOneController.m
+++ Test副本/Test/JJOneController.m
@@ -42,13 +42,12 @@
         [_viewScroll removeFromSuperview];
         [_textScroll removeFromSuperview];
         [self addScrollView];
-
+        NSLog(@"试下");
 
     } else if(button.tag == 2) {
         [_viewScroll removeFromSuperview];
         [_textScroll removeFromSuperview];
         if(_viewScroll != 0) {
-            NSLog(@"测试");
         }
     }
 
